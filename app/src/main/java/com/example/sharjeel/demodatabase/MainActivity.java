e7653b59fada9c7210733bd0a909b81e87dfdbf1
DigiconSharjeel <sharjeelhashmi.18@gmail.com>
2018-09-01 10:30:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sharjeel/demodatabase/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sharjeel.demodatabase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoregister(View view) {
+        Intent intent = new Intent(this,register.class);
+        startActivity(intent);
+    }
+
+}
